afdfce6cf7be414b97c97a4b9ab74b3c2948450b
unknown <mbattisti@MBATTISTI-PC.hars.local>
2019-05-10 17:27:08 +0200

    height of viewer and sidebar

--- src/app/my-second-view/my-second-view.component.ts
+++ src/app/my-second-view/my-second-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { NodesApiService, CardViewTextItemModel } from '@alfresco/adf-core';
 import { ActivatedRoute } from '@angular/router';
 import { MinimalNode } from '@alfresco/js-api';
@@ -12,15 +12,19 @@ import { IncompleteDocsService } from 'app/services/incomplete-docs.service';
 export class MySecondViewComponent implements OnInit, OnDestroy {
 
   private params: any;
-  idConfig : string;
+  idConfig: string;
   nodeId: string; // sample
-  
+  height = '600px';
   node: MinimalNode;
 
   // TODO
   // this need to be Dynamic
   properties = null;
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.height = document.querySelector('mat-sidenav-content').getBoundingClientRect().height + 'px';
+  }
 
   constructor(private nodeService: NodesApiService, private route: ActivatedRoute, private incompleteServ: IncompleteDocsService) {
     this.params = this.route.params.subscribe(params => {
@@ -28,36 +32,35 @@ export class MySecondViewComponent implements OnInit, OnDestroy {
       this.nodeId = params['nodeId'];
 
       // TODO
-      // call the node load and config load, 
+      // call the node load and config load,
       // do the parsing of the node propresties to populate the "" desired
-      let docConfig = this.incompleteServ.getConfigById(this.idConfig);
+      const docConfig = this.incompleteServ.getConfigById(this.idConfig);
       this.properties = [];
 
       this.properties.push(
         new CardViewTextItemModel({
-        label: 'File Name',
-        value: 'TODO',
-        key: 'fileName',
-        editable: false,
-        clickCallBack : ()=>{ }
+          label: 'File Name',
+          value: 'TODO',
+          key: 'fileName',
+          editable: false,
+          clickCallBack: () => { }
         })
       );
 
       docConfig.formFields.forEach(el => {
         this.properties.push(
           new CardViewTextItemModel({
-          label: el.label,
-          value: 'TODO',
-          key: el.key,
-          editable: true,
-          clickCallBack : ()=>{ }
+            label: el.label,
+            value: 'TODO',
+            key: el.key,
+            editable: true,
+            clickCallBack: () => { }
           })
         );
       });
-      
+
       console.log(this.properties);
-      
-      /** 
+      /**
       this.nodeService.getNode(this.nodeId).subscribe((entry: MinimalNode) => {
         this.node= entry;
       });
@@ -67,6 +70,7 @@ export class MySecondViewComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.onResize();
   }
 
   ngOnDestroy() {
