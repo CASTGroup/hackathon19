ac542e2866264bd7829cb7778fb5c3f9adb9db4e
unknown <mbattisti@MBATTISTI-PC.hars.local>
2019-05-10 16:45:51 +0200

    Adding preview component

--- src/app/my-first-view/my-first-view.component.ts
+++ src/app/my-first-view/my-first-view.component.ts
@@ -15,29 +15,29 @@ export class MyFirstViewComponent implements OnInit, OnDestroy {
 
   // TODO
   // this is a sample data, object must be returned from a query result
-  data = null
-  /** 
+  data = null;
+  /**
   new ObjectDataTableAdapter(
     [
       {
-        id: 1, 
-        fileName: "test.pdf", 
-        "demo:code": "Code #1", 
-        "demo:description": "Description #1", 
+        id: 1,
+        fileName: "test.pdf",
+        "demo:code": "Code #1",
+        "demo:description": "Description #1",
         "demo:value": "Value #1",
       },
       {
-        id: 2, 
-        fileName: "test.pdf", 
-        "demo:code": "Code #2", 
-        "demo:description": "Description #2", 
+        id: 2,
+        fileName: "test.pdf",
+        "demo:code": "Code #2",
+        "demo:description": "Description #2",
         "demo:value": "Value #2",
       },
       {
-        id: 3, 
-        fileName: "test.pdf", 
-        "demo:code": "Code #3", 
-        "demo:description": "Description #3", 
+        id: 3,
+        fileName: "test.pdf",
+        "demo:code": "Code #3",
+        "demo:description": "Description #3",
         "demo:value": "Value #3",
       },
     ]
@@ -46,18 +46,18 @@ export class MyFirstViewComponent implements OnInit, OnDestroy {
   schema = [];
 
   private params: any;
-  idConfig : string;
+  idConfig: string;
 
   config: DocumentsConfig;
 
   @Input()
-  showViewer: boolean = false;
-  
-  constructor(private nodeService: NodesApiService, 
-              private route: ActivatedRoute,
-              private incompleteServ: IncompleteDocsService,
-              private preview: PreviewService
-              ) {
+  showViewer = false;
+
+  constructor(private nodeService: NodesApiService,
+    private route: ActivatedRoute,
+    private incompleteServ: IncompleteDocsService,
+    private preview: PreviewService
+  ) {
   }
 
   ngOnInit() {
@@ -71,15 +71,15 @@ export class MyFirstViewComponent implements OnInit, OnDestroy {
         title: 'File',
         sortable: 'true'
       });
-      
+
       this.config.formFields.forEach(el => {
         this.schema.push({
           type: 'text',
           key: el.key,
           title: el.label,
           sortable: 'true'
-        } )
-      })
+        });
+      });
       console.log(this.schema);
       this.incompleteServ.getDocumentsByConfigId(this.idConfig).subscribe(
         (documentsFound) => {
@@ -93,7 +93,7 @@ export class MyFirstViewComponent implements OnInit, OnDestroy {
     // here we need to do the query, parse the results and prepare a ObjectDataTableAdapter ad-hoc
     // the term needs to be the config.query + all the requeried field in OR for the NULL check
 
-    /** 
+    /**
       this.nodeService.getNodeQueryResults(term: string, options?: SearchOptions).subscribe((entry: MinimalNode) => {
         this.node= entry;
       });
@@ -107,7 +107,7 @@ export class MyFirstViewComponent implements OnInit, OnDestroy {
   onRowClick(event: any) {
     // TODO
     // here we react to the node selected, than we are able to display the properties of the node
-    //alert('We just clicked row id: ' + event.value.obj.id);
+    // alert('We just clicked row id: ' + event.value.obj.id);
     console.log(event);
     this.preview.showResource(event.value.obj.id);
   }
